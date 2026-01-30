34e123a1e13e2656b68619599c906442b2d7c132
Fath-Repo <mfattah1215@gmail.com>
2026-01-30 17:38:52 +0700

    feat: add feature checkbox and delete button

--- to-do_list/assets/js/script.js
+++ to-do_list/assets/js/script.js
@@ -35,10 +35,44 @@ function addTask() {
 function showTask(task) {
 
     const listItem = document.createElement("li");
+    listItem.className = "flex justify-between bg-gray-200 rounded-lg p-2 mt-5"
 
-    listItem.textContent = task;
+    //add checkbox
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox";
+
+    //add delete 
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "bg-red-500 rounded-lg text-white px-4 py-2";
+    deleteButton.textContent = "delete"
+
+    //changing text content of li into span
+    const name = document.createElement("span");
+    name.textContent = task;
+    name.className = "flex items-center justify-center text-center"
+
+    //input all the elements into li element
+    listItem.appendChild(checkbox);
+    listItem.appendChild(name);
+    listItem.appendChild(deleteButton);
 
     taskList.appendChild(listItem);
+
+    deleteButton.addEventListener("click", function(){
+        taskList.removeChild(listItem);
+        tasks = tasks.filter(function(t) {
+            return t !== task
+        });
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    });
+
+    checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+            name.classList.add("line-through")
+        } else {
+            name.classList.remove("line-through")
+        };
+    });
 }
 
 // 6. add even listener for the button
